7e897c0675919acaa4dca23ad2426615f512ecc4
Hanieh Gharaee <gharaie.hanieh@gmail.com>
2024-11-06 09:50:14 +0330

    Add Product table and write new query

--- SQLQueryw3school.sql
+++ SQLQueryw3school.sql
@@ -1,14 +1,21 @@
 SELECT * FROM customer
-/* */
+/***************************/
 SELECT CustomerName,City
 FROM customer
-/* */
+/**********************************/
 SELECT Country FROM customer
 SELECT DISTINCT Country From customer /*Remove duplicates*/
 SELECT COUNT (DISTINCT Country) FROM customer;
-
+/**********************************/
 /*SELECT COUNT(*) AS DistinctCountries
 FROM (SELECT DISTINCT Country FROM customer)*/
-
+/**********************************/
 SELECT * FROM customer
 WHERE Country = 'Mexico';
+/**********************************/
+SELECT * FROM customer
+WHERE CustomerID=1; /*WHERE CustomerID > 80;*/
+/**********************************/
+SELECT * FROM Products
+ORDER BY Price;
+/**********************************/
